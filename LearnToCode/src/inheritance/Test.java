fe278d546659e30b04c096459d35b0ce25fd37fb
oleksiy84 <o_orlovskiy@ukr.net>
2017-07-10 01:01:12 +0300

    update

--- LearnToCode/src/inheritance/Test.java
+++ LearnToCode/src/inheritance/Test.java
@@ -42,94 +42,158 @@ class WithPrivateConstructor {
 	}
 }
 
-// Implicit super constructor WithPrivateConstructor()
+// Implicit(неявный) super constructor WithPrivateConstructor()
 // is not visible for default constructor.
 // Must define an explicit constructor
-//class TryInherit extends WithPrivateConstructor {
-//}
+// class TryInherit extends WithPrivateConstructor {
+// }
 
 /////////////////////////////////////////////////////////////////
-//Abstract vs Final
+// Abstract vs Final
 
-//The class AA can be either abstract or final, not both 
-//abstract final class AA{}
+// The class AA can be either abstract or final, not both
+// abstract final class AA{}
 
-/*
-class AA{
-	final void m() {}
+class AA {
+	final void m() {
+	}
 }
-class BB extends AA{
-	//Cannot override the final method from AA
-	void m() {}
-	//but can be overloaded
-	void m(int t) {System.out.println(t);}
-}
-*/
 
-/*
-class AA{
+class BB extends AA {
+	// Cannot override the final method from AA
+	// void m() {}
+	// but can be overloaded
+	void m(int t) {
+		System.out.println(t);
+	}
+}
+
+//////////////////////////////////
+class A1 {
 	static void m() {
 		System.out.println("AA");
 	}
 }
-class BB extends AA{
-	//This instance method cannot override the static method from AA
-	void m() {}
-	//but can be overloaded
-	void m(int t) {System.out.println(t);}
-	//or can be shaded  own static method
-	static void m() {System.out.println("BB");}
-}
-*/
 
-//static vs final
+class B1 extends A1 {
+	// This instance method cannot override the static method from AA and vice-versa
+	// void m() {
+	// }
 
-/*
-//Illegal modifier for the class AA; 
-//only public, abstract & final are permitted
-static class AA{}
-*/
+	// but can be overloaded
+	void m(int t) {
+		System.out.println(t);
+	}
 
-/*
-//static final on methods can be
- class AA{
-	static final void m() {}
+	// or can be shaded own static method
+	static void m() {
+		System.out.println("B1");
+	}
 }
-class BB extends AA{
-	//can be overloading
-	static final void m(int y) {}
-	//void m(int y) {}
+//////////////////////////////////////////////////
+// static vs final
+
+// Illegal modifier for the class AA; //only public, abstract & final are
+// permitted
+// static class Aq{}
+
+// static final on methods can be
+class A4 {
+	static final void m() {
+	}
 }
-*/
+
+class B4 extends A4 {
+	// can be overloading
+	static final void m(int y) {
+	}
+	// or this
+	// void m(int y) {
+	// }
+}
 
 ///////////////////////////////////////////
-/*
-interface Inter{
-	public static final int P=0;
-	public abstract void method();
-	public static interface InnerInterface{}
-	public static class InnerClass{}
-}
-interface I2 extends Inter{}
-*/
-////////////////////////////////////////////
-/*
-class AA{
-	Object a() {
-		return "AA";
-	}
-}
-class BB extends AA{
-	//Cannot reduce the visibility of the inherited method from AA
-	@Override
-	private String a() {
-		return "BB";
-	}
-	public static void main(String[] args) {
-		AA aa = new BB();
-		System.out.println(aa.a()); //BB
-				
-	}
-}
-*/
 
+interface Inter {
+	public static final int P = 0;
+
+	public abstract void method();
+
+	public static interface InnerInterface {
+	}
+
+	public static class InnerClass {
+	}
+}
+
+interface I2 extends Inter {
+}
+
+////////////////////////////////////////////
+class TestClass implements T1, T2 {
+
+	@Override
+	public void m1() {
+	}
+
+	public static void main(String[] args) {
+		TestClass t = new TestClass();
+		t.m1();
+		// The static field T2.VALUE should be accessed in a static way (no error)
+		System.out.println(((T2) t).VALUE);
+		System.out.println(T1.VALUE);
+	}
+}
+
+interface T1 {
+	int VALUE = 1;
+
+	void m1();
+}
+
+interface T2 {
+	int VALUE = 2;
+
+	void m1();
+}
+/////////////////////////////////////////////////////
+
+class A2 {
+	Object a() {
+		return "A2";
+	}
+}
+
+class B2 extends A2 {
+
+	// if it was private - Cannot reduce the visibility of the inherited method from
+	// AA
+	@Override
+	Integer a() {
+		return 15;
+	}
+
+	public static void main(String[] args) {
+		A2 aa = new B2();
+		System.out.println(aa.a()); // 15
+	}
+}
+
+//////////////////////////////////////////
+class AAA {
+	int a() {
+		return 3;
+	}
+}
+
+class BBB extends AAA {
+	// The return type is incompatible with AA.a()
+	// @Override
+	// Integer a() {return 3;} //The return type is incompatible with AA.a()
+	// ONLY int
+	// @Override
+	// double a() {
+	// return 1.;
+	// }
+
+}
