293edcec7a1e6f4a60195a51a941e9acbaf590f3
oleksiy84 <o_orlovskiy@ukr.net>
2017-08-12 22:57:21 +0300

    xml

---
+++ LearnToCode/src/xml/Test.java
@@ -0,0 +1,14 @@
+package xml;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+	 private static final Logger log = 
+			 LoggerFactory.getLogger(Test.class);
+	public static void main(String[] args) {
+		log.info("Hello");
+
+	}
+
+}
