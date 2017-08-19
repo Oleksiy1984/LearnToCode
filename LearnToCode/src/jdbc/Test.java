3d7f72f8f35047780e713098b6169872e17f7466
oleksiy84 <o_orlovskiy@ukr.net>
2017-08-19 13:50:17 +0300

    о

--- LearnToCode/src/jdbc/Test.java
+++ LearnToCode/src/jdbc/Test.java
@@ -17,6 +17,9 @@ public class Test {
 	public static void main(String[] args) throws SQLException {
 		
 		//1. Загрузка класса драйвера
+		/*
+		 * Загрузка класса драйвера.
+		 */
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
